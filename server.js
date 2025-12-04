e307ad71a2959013e8ba4832e446f935030618dd
kaloyanvp <149475860+kaloyanvp@users.noreply.github.com>
2025-12-04 14:51:56 +0200

    Overhaul of how the token is generated

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT;
 const api_host = process.env.API_HOST;
-const usersRouter = require('./routes/usersRouter');
+const uploadRouter = require('./routes/uploadRouter');
 app.use(cors());
 app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ limit: '100mb', extended: true }));
@@ -21,20 +21,8 @@ app.get("/", (req, res) => {
     res.end(response);
 });
 
-app.use("/users", usersRouter);
+app.use("/upload", uploadRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://${api_host}:${port}`);
 });
-
-
-
-// const server = createServer((req, res) => {
-//   res.statusCode = 200;
-//   res.setHeader('Content-Type', 'text/plain');
-//   res.end('Hello World');
-// });
-
-// server.listen(port, hostname, () => {
-//   console.log(`Server running at http://${hostname}:${port}/`);
-// });
